3831b807c105d7df8b767642248f32a09b2a5866
moucheng2017 <moucheng.xu.18@ucl.ac.uk>
2022-01-13 14:03:41 +0000

    new testing looping through the whole volume

--- Utils.py
+++ Utils.py
@@ -17,7 +17,7 @@ import glob
 from scipy import ndimage
 import random
 
-from skimage import exposure
+# from skimage import exposure
 
 from Metrics import segmentation_scores, hd95, preprocessing_accuracy, f1_score
 from PIL import Image
@@ -79,6 +79,10 @@ def ensemble_segmentation(model_path, volume, train_size=[192, 192, 192], class_
     else:
         _, segmentation = torch.max(segmentation, dim=1)
     return segmentation
+
+
+# def ensemble_test_all():
+
 
 
 def sigmoid_rampup(current, rampup_length, limit):
@@ -155,7 +159,7 @@ def evaluate(validateloader, model, device, model_name, class_no, dilation):
     return validate_iou, validate_h_dist
 
 
-def test(saved_information_path, saved_model_path, testdata, device, model_name, class_no, dilation=1):
+def test(saved_information_path, saved_model_path, test_data_path, test_label_path, device, model_name, class_no, size=[192, 192, 192], dilation=1):
 
     save_path = saved_information_path + '/results'
     try:
@@ -165,107 +169,22 @@ def test(saved_information_path, saved_model_path, testdata, device, model_name,
             raise
         pass
 
-    all_models = [os.path.join(saved_model_path, f) for f in listdir(saved_model_path) if os.path.isfile(os.path.join(saved_model_path, f))]
-    all_models.sort()
+    all_cases = [os.path.join(test_data_path, f) for f in listdir(test_data_path)]
+    all_labels = [os.path.join(test_label_path, f) for f in listdir(test_label_path)]
 
-    # all_cases = [os.path.join(testdata, case)]
-
-    # all_models = glob.glob(os.path.join(saved_model_path, '*.pt'))
-    # print(all_models)
-
-    # testing acc with main decoder:
     test_iou = []
-    # test_iou_wt = []
-    # test_iou_et = []
-    # test_iou_tc = []
-    test_h_dist = []
-    # test_recall = []
-    # test_precision = []
 
+    for each_case, each_label in zip(all_cases, all_labels):
 
+        volume = np.load(each_case)
+        label = np.load(each_label)
 
-    # for model in all_models:
-    #     # test_time = 0
-    #     model = torch.load(model)
-    #     model.eval()
-    #
-    #     start = timeit.default_timer()
-    #     with torch.no_grad():
-    #
-    #         for ii, (test_images, test_label, test_imagename) in enumerate(testdata):
-    #             test_img = test_images.to(device=device, dtype=torch.float32)
-    #             # test_img = test_images.to(device=device, dtype=torch.float32).unsqueeze(1)
-    #             test_label = test_label.to(device=device, dtype=torch.float32)
-    #
-    #             assert torch.max(test_label) != 100.0
-    #
-    #             if 'CCT' in model_name or 'cct' in model_name:
-    #                 test_outputs, _ = model(test_img)
-    #             elif 'expert' in model_name:
-    #                 test_outputs = model(test_img, dilation)
-    #             else:
-    #                 test_outputs = model(test_img)
-    #
-    #             if class_no == 2:
-    #                 test_class_outputs = torch.sigmoid(test_outputs)
-    #                 test_class_outputs = (test_class_outputs > 0.5).float()
-    #             else:
-    #                 _, test_class_outputs = torch.max(test_outputs, dim=1)
-    #
-    #             # testing on average metrics:
-    #             test_label = test_label.squeeze()
-    #             test_class_outputs = test_class_outputs.squeeze()
-    #
-    #             # # whole tumour: 1 == 1, 2 == 1, 3 == 1
-    #             # test_label_wt = torch.zeros_like(test_label)
-    #             # test_class_outputs_wt = torch.zeros_like(test_class_outputs)
-    #             # test_label_wt[test_label == 1] = 1
-    #             # test_label_wt[test_label == 2] = 1
-    #             # test_label_wt[test_label == 3] = 1
-    #             # test_class_outputs_wt[test_class_outputs == 1] = 1
-    #             # test_class_outputs_wt[test_class_outputs == 2] = 1
-    #             # test_class_outputs_wt[test_class_outputs == 3] = 1
-    #             # test_mean_iu_wt_ = segmentation_scores(test_label_wt.squeeze(), test_class_outputs_wt.squeeze(), 2)
-    #             # test_iou_wt.append(test_mean_iu_wt_)
-    #             #
-    #             # # enhancing tumour core: 3 == 1, 1 == 0, 2 == 0
-    #             # test_label_et = torch.zeros_like(test_label)
-    #             # test_class_outputs_et = torch.zeros_like(test_class_outputs)
-    #             # test_label_et[test_label == 3] = 1
-    #             # test_label_et[test_label == 1] = 0
-    #             # test_label_et[test_label == 2] = 0
-    #             # test_class_outputs_et[test_class_outputs == 3] = 1
-    #             # test_class_outputs_et[test_class_outputs == 1] = 0
-    #             # test_class_outputs_et[test_class_outputs == 2] = 0
-    #             # test_mean_iu_et_ = segmentation_scores(test_label_wt.squeeze(), test_class_outputs_wt.squeeze(), 2)
-    #             # test_iou_et.append(test_mean_iu_et_)
-    #             #
-    #             # # tumour core: 3 == 1, 1 == 1, 2 == 0
-    #             # test_label_tc = torch.zeros_like(test_label)
-    #             # test_class_outputs_tc = torch.zeros_like(test_class_outputs)
-    #             # test_label_tc[test_label == 3] = 1
-    #             # test_label_tc[test_label == 1] = 1
-    #             # test_label_tc[test_label == 2] = 0
-    #             # test_class_outputs_tc[test_class_outputs == 3] = 1
-    #             # test_class_outputs_tc[test_class_outputs == 1] = 1
-    #             # test_class_outputs_tc[test_class_outputs == 2] = 0
-    #             # test_mean_iu_tc_ = segmentation_scores(test_label_tc.squeeze(), test_class_outputs_tc.squeeze(), 2)
-    #             # test_iou_tc.append(test_mean_iu_tc_)
-    #
-    #             # test_mean_iu_ = (test_mean_iu_tc_ + test_mean_iu_et_ + test_mean_iu_wt_) / 3
-    #             test_mean_iu_ = segmentation_scores(test_label.squeeze(), test_class_outputs.squeeze(), 2)
-    #             test_mean_f1_, test_mean_recall_, test_mean_precision_ = f1_score(test_label.squeeze(), test_class_outputs.squeeze(), class_no)
-    #
-    #             test_iou.append(test_mean_iu_)
-    #             test_recall.append(test_mean_recall_)
-    #             test_precision.append(test_mean_precision_)
+        segmentation = ensemble_segmentation(saved_model_path, volume, train_size=size, class_no=class_no)
+        test_mean_iu_ = segmentation_scores(label.squeeze(), segmentation.squeeze(), class_no)
+        test_iou.append(test_mean_iu_)
 
-                # if (test_class_outputs == 1).sum() > 1 and (test_label == 1).sum() > 1:
-                #     t_dist_ = hd95(test_class_outputs.squeeze(), test_label.squeeze(), class_no)
-                #     test_h_dist.append(t_dist_)
-
-        stop = timeit.default_timer()
-        test_time = stop - start
+        # stop = timeit.default_timer()
+        # test_time = stop - start
 
     # print(len(testdata))
     result_dictionary = {
@@ -279,10 +198,10 @@ def test(saved_information_path, saved_model_path, testdata, device, model_name,
         # 'Test IoU tc std': str(np.nanstd(test_iou_tc)),
         # 'Test H-dist mean': str(np.nanmean(test_h_dist)),
         # 'Test H-dist std': str(np.nanstd(test_h_dist)),
-        'Test recall mean': str(np.nanmean(test_recall)),
-        'Test recall std': str(np.nanstd(test_recall)),
-        'Test precision mean': str(np.nanmean(test_precision)),
-        'Test precision std': str(np.nanstd(test_precision)),
+        # 'Test recall mean': str(np.nanmean(test_recall)),
+        # 'Test recall std': str(np.nanstd(test_recall)),
+        # 'Test precision mean': str(np.nanmean(test_precision)),
+        # 'Test precision std': str(np.nanstd(test_precision)),
         # 'Training time(s)': str(training_time),
         # 'Test time(s)': str(test_time / len(testdata))
     }
@@ -293,21 +212,12 @@ def test(saved_information_path, saved_model_path, testdata, device, model_name,
     ff.close()
 
     iou_path = save_path + '/iou.csv'
-    # iou_tc_path = save_path + '/iou_tc.csv'
-    # iou_wt_path = save_path + '/iou_wt.csv'
-    # iou_et_path = save_path + '/iou_et.csv'
-
-    # h_dist_path = save_path + '/h_dist.csv'
-    recall_path = save_path + '/recall.csv'
-    precision_path = save_path + '/precision.csv'
+    # recall_path = save_path + '/recall.csv'
+    # precision_path = save_path + '/precision.csv'
 
     np.savetxt(iou_path, test_iou, delimiter=',')
-    # np.savetxt(iou_wt_path, test_iou_wt, delimiter=',')
-    # np.savetxt(iou_tc_path, test_iou_tc, delimiter=',')
-    # np.savetxt(iou_et_path, test_iou_et, delimiter=',')
-    # np.savetxt(h_dist_path, test_h_dist, delimiter=',')
-    np.savetxt(recall_path, test_recall, delimiter=',')
-    np.savetxt(precision_path, test_precision, delimiter=',')
+    # np.savetxt(recall_path, test_recall, delimiter=',')
+    # np.savetxt(precision_path, test_precision, delimiter=',')
 
     return test_iou
 
