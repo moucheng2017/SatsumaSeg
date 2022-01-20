8ca8208085d6ab7cf498d41901dc7585753b4c0c
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-01-20 20:24:30 +0000

    2021 01 20e: Testing limit of memory

--- Run_test.py
+++ Run_test.py
@@ -13,14 +13,14 @@ if __name__ == '__main__':
     trainModels(data_directory='/home/moucheng/projects_data/Pulmonary_data/',
                 dataset_name='airway',
                 dataset_tag='mismatch_exp',
-                downsample=0,
+                downsample=4,
                 input_dim=1,
                 class_no=2,
                 repeat=1,
                 train_batchsize=1,
                 num_steps=1600,
-                learning_rate=1e-3,
-                width=12,
+                learning_rate=2e-4,
+                width=16,
                 log_tag='20220120_no_data_augmentation',
-                new_resolution=[10, 224, 224]
+                new_resolution=[32, 384, 384]
                 )
