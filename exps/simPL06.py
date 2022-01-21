9a0864eaaa09c827a4b4a05fd9077cc5fcc05ccf
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-01-21 10:42:15 +0000

    adding a few exps with long training time at 512 x 512

---
+++ exps/simPL06.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append("..")
+from Train_Unet_simPL import trainModels
+
+if __name__ == '__main__':
+
+    trainModels(data_directory='/SAN/medic/PerceptronHead/data/lung/public/',
+                dataset_name='airway',
+                dataset_tag='mismatch_exp',
+                downsample=0,
+                input_dim=1,
+                class_no=2,
+                repeat=3,
+                train_batchsize=1,
+                num_steps=36000,
+                learning_rate=1e-3,
+                width=16,
+                log_tag='20220121',
+                new_resolution=[8, 512, 512]
+                )
