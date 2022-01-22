0c9a58111edeb38740ba59dee0cfdbb3c2a0e02d
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-01-22 11:45:26 +0000

    2021 01 21 before curating the data set

--- Train_Unet_simPL.py
+++ Train_Unet_simPL.py
@@ -229,8 +229,8 @@ def trainSingleModel(model,
             loss.backward()
             optimizer.step()
 
-            for param_group in optimizer.param_groups:
-                param_group["lr"] = learning_rate * ((1 - float(step) / num_steps) ** 0.99)
+            # for param_group in optimizer.param_groups:
+            #     param_group["lr"] = learning_rate * ((1 - float(step) / num_steps) ** 0.99)
 
             print(
                 'Step [{}/{}], '
