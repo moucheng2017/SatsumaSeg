a6ed60244b50532b40ec1fe5c3f066a4116020bb
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-01-25 20:27:16 +0000

    512 x 512 again

--- qsub/base03.sh
+++
@@ -1,8 +0,0 @@
-#$ -l tmem=12G
-#$ -l gpu=true
-#$ -S /bin/bash
-#$ -j y
-#$ -l h_rt=30:00:00
-#$ -wd /SAN/medic/PerceptronHead/codes/SatsumaSeg/exps
-
-~/anaconda3/envs/env2021/bin/python base03.py
