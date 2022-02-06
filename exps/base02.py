1436f24d9cfa648dfb3e4b53e6eeeedd4b3f336c
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-02-06 21:50:11 +0000

    adding new experiemtns

---
+++ exps/base02.py
@@ -0,0 +1,27 @@
+import sys
+import torch
+sys.path.append("..")
+
+torch.manual_seed(0)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+from Train_Unet import trainModels
+
+if __name__ == '__main__':
+
+    trainModels(data_directory='/SAN/medic/PerceptronHead/data/lung/private/',
+                dataset_name='airway',
+                dataset_tag='Mixed',
+                downsample=4,
+                input_dim=1,
+                class_no=2,
+                repeat=1,
+                train_batchsize=2,
+                num_steps=4000,
+                learning_rate=1e-4,
+                width=16,
+                log_tag='20200206',
+                new_resolution=[16, 256, 256],
+                l2=5e-2
+                )
---
+++ qsub/simPL02.sh
@@ -0,0 +1,8 @@
+#$ -l tmem=16G
+#$ -l gpu=true,gpu_type=rtx6000
+#$ -S /bin/bash
+#$ -j y
+#$ -l h_rt=48:00:00
+#$ -wd /SAN/medic/PerceptronHead/codes/SatsumaSeg/exps
+
+~/miniconda3/envs/pytorch1.4/bin/python simPL02.py
