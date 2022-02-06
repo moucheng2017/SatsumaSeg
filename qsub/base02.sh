ec73ba443c2ac7e6bef0ad4cefea08fd7dfe3a70
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-02-06 21:25:34 +0000

    trying to use antoerh  gpu

--- qsub/base02.sh
+++
@@ -1,8 +0,0 @@
-#$ -l tmem=16G
-#$ -l gpu=true,gpu_type=(p100|v100)
-#$ -S /bin/bash
-#$ -j y
-#$ -l h_rt=48:00:00
-#$ -wd /SAN/medic/PerceptronHead/codes/SatsumaSeg/exps
-
-~/miniconda3/envs/pytorch1.4/bin/python base02.py
