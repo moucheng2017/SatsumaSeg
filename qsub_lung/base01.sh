015944616c83b622c8734497b385ef77aaa282f1
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-02-19 19:27:58 +0000

    New Learning

--- qsub_lung/base01.sh
+++
@@ -1,8 +0,0 @@
-#$ -l tmem=16G
-#$ -l gpu=true,gpu_type=!(gtx1080ti|rtx2080ti)
-#$ -S /bin/bash
-#$ -j y
-#$ -l h_rt=72:00:00
-#$ -wd /SAN/medic/PerceptronHead/codes/SatsumaSeg/exps_lung
-
-~/miniconda3/envs/pytorch1.4/bin/python base01.py
