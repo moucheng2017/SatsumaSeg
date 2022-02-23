c446edd8934dbdc7e3b3eb6a1289e90fa3849588
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-02-23 16:48:32 +0000

    debugging the NAN value, sometimes some slices are only background so the threshold value can be nan...

--- Train_Unet_simPL_EM.py
+++ Train_Unet_simPL_EM.py
@@ -222,9 +222,12 @@ def trainSingleModel(model,
             # Calculate the threshold via expecation:
             threshold_u_ = torch.sigmoid(outputs_u.detach() / temperature)
             threshold_u_ = threshold_u_[threshold_u_ > threshold_u].mean()
-            threshold_u = 0.9*threshold_u + 0.1*threshold_u_
-            # threshold_u = torch.min(threshold_u, torch.tensor(0.999).to(device))
-            learnt_pseudo_label = (prob_outputs_u.detach() >= threshold_u).float()
+
+            if torch.isnan(threshold_u_) is False:
+                threshold_u = 0.9*threshold_u + 0.1*threshold_u_
+                learnt_pseudo_label = (prob_outputs_u.detach() > threshold_u).float()
+            else:
+                learnt_pseudo_label = (prob_outputs_u.detach() > threshold_u).float()
 
             if class_no == 2:
                 loss = SoftDiceLoss()(prob_outputs, labels) + nn.BCELoss(reduction='mean')(prob_outputs.squeeze(), labels.squeeze())
