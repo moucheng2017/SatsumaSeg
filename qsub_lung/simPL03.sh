7a5960feda8ee4f3765dcc1c4e3708218e4c7d1e
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-02-24 22:48:56 +0000

    2021 02 24 b

---
+++ qsub_lung/simPL03.sh
@@ -0,0 +1,8 @@
+#$ -l tmem=16G
+#$ -l gpu=true,gpu_type=(rtx8000)
+#$ -S /bin/bash
+#$ -j y
+#$ -l h_rt=48:00:00
+#$ -wd /SAN/medic/PerceptronHead/codes/SatsumaSeg/exps_lung
+
+~/miniconda3/envs/pytorch1.4/bin/python simPL03.py
