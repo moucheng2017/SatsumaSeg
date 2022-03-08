ea862402f0ac92fea54d38daf93fa14a26db3bb5
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-03-08 21:46:10 +0000

    adding qsub

--- qsub_airway/simPL02.sh
+++ qsub_airway/simPL02.sh
@@ -1,8 +1,8 @@
 #$ -l tmem=16G
-#$ -l gpu=true,gpu_type=rtx6000
+#$ -l gpu=true,gpu_type=(rtx8000)
 #$ -S /bin/bash
 #$ -j y
-#$ -l h_rt=48:00:00
+#$ -l h_rt=96:00:00
 #$ -wd /SAN/medic/PerceptronHead/codes/SatsumaSeg/exps_airway
 
 ~/miniconda3/envs/pytorch1.4/bin/python simPL02.py
