1c1ae5bb2344b2e49427418828d1c1a6dd284401
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-03-09 07:59:08 +0000

    reducing batch size and depth to 16

--- exps_airway/base01.py
+++ exps_airway/base01.py
@@ -16,11 +16,11 @@ if __name__ == '__main__':
                 input_dim=1,
                 class_no=2,
                 repeat=1,
-                train_batchsize=2,
+                train_batchsize=1,
                 num_steps=5000,
-                learning_rate=1e-2,
-                width=32,
-                log_tag='20200206',
-                new_resolution=[32, 480, 480],
+                learning_rate=5e-3,
+                width=16,
+                log_tag='220200309',
+                new_resolution=[16, 480, 480],
                 l2=1e-4
                 )
