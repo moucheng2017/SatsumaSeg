3e681234a3a5c4b01ba24b3290d1c214329da41e
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-03-25 19:38:15 +0000

    2021 03 25 c

--- Run_test.py
+++ Run_test.py
@@ -1,7 +1,7 @@
 import torch
 # sys.path.append("..")
-# from Train_Unet_SegPLVI import trainModels
-from Train_Unet import trainModels
+from Train_Unet_SegPLVI import trainModels
+# from Train_Unet import trainModels
 # torch.manual_seed(0)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
@@ -9,26 +9,6 @@ torch.backends.cudnn.benchmark = False
 if __name__ == '__main__':
     # with torch.autograd.set_detect_anomaly(True):
     # SegPL-VI:
-    # trainModels(data_directory='/home/moucheng/projects_data/Pulmonary_data',
-    #             dataset_name='airway',
-    #             downsample=4,
-    #             input_dim=1,
-    #             class_no=2,
-    #             repeat=1,
-    #             train_batchsize=1,
-    #             num_steps=2000,
-    #             learning_rate=1e-4,
-    #             width=32,
-    #             log_tag='airway_segpl_vi_pat_2d',
-    #             unlabelled=2,
-    #             new_resolution=[1, 480, 480],
-    #             l2=1e-4,
-    #             alpha=0.1,
-    #             warmup=1.0,
-    #             mean=0.4,
-    #             std=0.12
-    #             )
-
     trainModels(data_directory='/home/moucheng/projects_data/Pulmonary_data',
                 dataset_name='airway',
                 downsample=4,
@@ -36,10 +16,30 @@ if __name__ == '__main__':
                 class_no=2,
                 repeat=1,
                 train_batchsize=5,
-                num_steps=2000,
+                num_steps=3000,
                 learning_rate=1e-3,
                 width=64,
                 log_tag='airway_balanced',
+                unlabelled=2,
                 new_resolution=[1, 480, 480],
-                l2=1e-2
+                l2=1e-2,
+                alpha=1.0,
+                warmup=1.0,
+                mean=0.4,
+                std=0.12
                 )
+
+    # trainModels(data_directory='/home/moucheng/projects_data/Pulmonary_data',
+    #             dataset_name='airway',
+    #             downsample=4,
+    #             input_dim=1,
+    #             class_no=2,
+    #             repeat=1,
+    #             train_batchsize=5,
+    #             num_steps=2000,
+    #             learning_rate=1e-3,
+    #             width=64,
+    #             log_tag='airway_balanced',
+    #             new_resolution=[1, 480, 480],
+    #             l2=1e-2
+    #             )
