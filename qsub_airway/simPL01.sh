342aafa17e23440a3c1a735743365e718d79f3d7
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-03-27 16:17:22 +0100

    2021 03 27 a

--- qsub_airway/simPL01.sh
+++ qsub_airway/simPL01.sh
@@ -2,7 +2,7 @@
 #$ -l gpu=true,gpu_type=(rtx8000)
 #$ -S /bin/bash
 #$ -j y
-#$ -l h_rt=96:00:00
+#$ -l h_rt=120:00:00
 #$ -wd /SAN/medic/PerceptronHead/codes/SatsumaSeg/exps_airway
 
 ~/miniconda3/envs/pytorch1.4/bin/python simPL01.py
