6c06dfdd5d687b39740c0da6f01b18059995305d
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-04-07 09:45:37 +0100

    changing batch size

--- exps_airway/simPL06.py
+++ exps_airway/simPL06.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
                 input_dim=1,
                 class_no=2,
                 repeat=1,
-                train_batchsize=3,
+                train_batchsize=2,
                 num_steps=3000,
                 learning_rate=1e-3,
                 width=64,
