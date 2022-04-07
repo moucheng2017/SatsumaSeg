d2f22c235a2c05ebedbe80f30c4747c6e768d730
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-04-07 09:16:06 +0100

    two experiments

--- exps_airway/base02.py
+++ exps_airway/base02.py
@@ -6,26 +6,21 @@ torch.manual_seed(0)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-from Train_Unet_SegPLVI import trainModels
+from Train_Unet import trainModels
 
 if __name__ == '__main__':
 
     trainModels(data_directory='/SAN/medic/PerceptronHead/data/lung/private/',
                 dataset_name='airway',
-                downsample=0,
+                downsample=2,
                 input_dim=1,
                 class_no=2,
                 repeat=1,
-                train_batchsize=3,
+                train_batchsize=5,
                 num_steps=3000,
                 learning_rate=1e-3,
                 width=64,
-                unlabelled=1,
                 log_tag='220200309',
-                new_resolution=[3, 480, 480],
-                l2=1e-2,
-                alpha=1.0,
-                warmup=1.0,
-                mean=0.5,
-                std=0.12
+                new_resolution=[8, 480, 480],
+                l2=1e-2
                 )
