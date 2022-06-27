d3237a68c3f907dc69ba88eb632a51ae10f6d6ff
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-06-27 11:09:40 +0100

    2021 06 25 d: use single slice and stuff

--- qsub_airway/base34.sh
+++ qsub_airway/base34.sh
@@ -1,8 +1,8 @@
-#$ -l tmem=24G
+#$ -l tmem=16G
 #$ -l gpu=true,gpu_type=!(gtx1080ti|rtx2080ti)
 #$ -S /bin/bash
 #$ -j y
-#$ -l h_rt=504:00:00
+#$ -l h_rt=400:00:00
 #$ -wd /SAN/medic/PerceptronHead/codes/SatsumaSeg/exps_airway
 
 ~/miniconda3/envs/pytorch1.4/bin/python base34.py
