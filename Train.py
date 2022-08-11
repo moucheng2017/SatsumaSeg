32d7d1b56b5f67b3f68aa5f87652acbc9cb1c02c
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-08-11 17:20:34 +0100

    2021 08 11a: now merging train scripts together

---
+++ Train.py
@@ -0,0 +1,29 @@
+# basic libs:
+import math
+import torch
+import timeit
+import shutil
+from pathlib import Path
+
+# Deterministic training:
+import random
+import numpy as np
+import torch.backends.cudnn as cudnn
+
+# Tracking the training process:
+# import wandb
+from tensorboardX import SummaryWriter
+
+# model:
+from Models2D import Unet, UnetBPL
+
+# training options control panel:
+from TrainArguments import parser
+
+# training data loader:
+from libs.DataloaderOrthogonal import getData
+
+from libs import TrainBase
+
+
+# class
