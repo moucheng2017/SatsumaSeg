b476e159fda35f724f855b65d35aa27bb7cf883d
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-11-08 17:45:35 +0000

    2021 11 08 b: debugging01: wrong run file before

--- cluster/hip01.sh
+++ cluster/hip01.sh
@@ -5,4 +5,4 @@
 #$ -l h_rt=24:00:00
 #$ -wd /SAN/medic/PerceptronHead/codes/SatsumaSeg/
 
-~/miniconda3/envs/pytorch1.4/bin/python Run.py --data '/SAN/medic/PerceptronHead/data/hipct/hip_covid' --log_tag 'exp' --iterations 5000 --lr 0.01 --depth 4 --width 16 --batch 1 --unlabelled 1 --mu 0.9 --sigma 0.1 --full_orthogonal 0 --zoom 1 --sampling 5 --new_size_h 512 --new_size_w 512 --detach 0 --saving_starting 50
+~/miniconda3/envs/pytorch1.4/bin/python RunHip.py --data '/SAN/medic/PerceptronHead/data/hipct/hip_covid' --log_tag 'exp' --iterations 5000 --lr 0.01 --depth 4 --width 16 --batch 1 --unlabelled 1 --mu 0.9 --sigma 0.1 --full_orthogonal 0 --zoom 1 --sampling 5 --new_size_h 512 --new_size_w 512 --detach 0 --saving_starting 50
