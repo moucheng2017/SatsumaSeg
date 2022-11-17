2021 11 17 b: debugged sup and unsup kl still wrong but that's okay

MainSemi.py
@@ -6,6 +6,8 @@ import shutil
 from libs.Train import train_semi
 from libs import Helpers
 
+from libs.Validate import validate
+
 
 def trainBPL(args):
     # fix a random seed:
@@ -13,6 +15,7 @@ def trainBPL(args):
 
     # model intialisation:
     model, model_name = Helpers.network_intialisation(args)
+    model_ema, _ = Helpers.network_intialisation(args)
 
     # resume training:
     if args.resume == 1:
@@ -20,6 +23,7 @@ def trainBPL(args):
 
     # put model in the gpu:
     model.cuda()
+    model_ema.cuda()
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-8, weight_decay=args.l2)
 
     # make saving directories:
@@ -35,19 +39,35 @@ def trainBPL(args):
     train_labelled_data_loader = data_iterators.get('train_loader_l')
     iterator_train_labelled = iter(train_labelled_data_loader)
 
+    # validate labelled:
+    val_labelled_data_loader = data_iterators.get('val_loader_l')
+
     # train unlabelled:
     train_unlabelled_data_loader = data_iterators.get('train_loader_u')
     iterator_train_unlabelled = iter(train_unlabelled_data_loader)
 
+    # initialisation of best acc tracker
+    best_val = 0.0
+
+    # initialisation of counter for ema avg:
+    ema_count = 0
+
+    # initialisation of growth of val acc tracker
+    best_val_count = 1
+
     # running loop:
     for step in range(args.iterations):
-        
+
+        # initialisation of validating acc:
+        validate_acc = 0.0
+
         # ramp up alpha and beta:
-        current_alpha = Helpers.ramp_up(args.alpha, args.warmup, step, args.iterations)
-        current_beta = Helpers.ramp_up(args.beta, args.warmup, step, args.iterations)
+        current_alpha = Helpers.ramp_up(args.alpha, args.warmup, step, args.iterations, args.warmup_start)
+        current_beta = Helpers.ramp_up(args.beta, args.warmup, step, args.iterations, args.warmup_start)
 
         # put model to training mode:
         model.train()
+        model_ema.train()
 
         # labelled data
         labelled_dict = Helpers.get_data_dict(train_labelled_data_loader, iterator_train_labelled)
@@ -62,8 +82,9 @@ def trainBPL(args):
                                 model=model,
                                 t=args.temp,
                                 prior_mu=args.mu,
-                                prior_logsigma=args.sigma,
-                                augmentation_cutout=args.cutout)
+                                # prior_logsigma=args.sigma,
+                                augmentation_cutout=args.cutout
+                                )
 
             loss_h = train_semi(labelled_img=labelled_dict["plane_h"][0],
                                 labelled_label=labelled_dict["plane_h"][1],
@@ -71,8 +92,9 @@ def trainBPL(args):
                                 model=model,
                                 t=args.temp,
                                 prior_mu=args.mu,
-                                prior_logsigma=args.sigma,
-                                augmentation_cutout=args.cutout)
+                                # prior_logsigma=args.sigma,
+                                augmentation_cutout=args.cutout
+                                )
 
             loss_w = train_semi(labelled_img=labelled_dict["plane_w"][0],
                                 labelled_label=labelled_dict["plane_w"][1],
@@ -80,19 +102,23 @@ def trainBPL(args):
                                 model=model,
                                 t=args.temp,
                                 prior_mu=args.mu,
-                                prior_logsigma=args.sigma,
-                                augmentation_cutout=args.cutout)
+                                # prior_logsigma=args.sigma,
+                                augmentation_cutout=args.cutout
+                                )
 
             sup_loss = loss_d.get('supervised loss').get('loss') + loss_h.get('supervised loss').get('loss') + loss_w.get('supervised loss').get('loss')
             sup_loss = sup_loss / 3
             # print(sup_loss)
+
+            train_iou = loss_d.get('supervised loss').get('train iou') + loss_h.get('supervised loss').get('train iou') + loss_w.get('supervised loss').get('train iou')
+            train_iou = train_iou / 3
 
             pseudo_loss = loss_d.get('pseudo loss').get('loss') + loss_h.get('pseudo loss').get('loss') + loss_w.get('pseudo loss').get('loss')
             pseudo_loss = current_alpha*pseudo_loss / 3
             # print(pseudo_loss)
 
             kl_loss = loss_d.get('kl loss').get('loss') + loss_h.get('kl loss').get('loss') + loss_w.get('kl loss').get('loss')
-            kl_loss = current_beta*kl_loss / 3
+            kl_loss = current_alpha*current_beta*kl_loss / 3
             # print(kl_loss)
 
             loss = sup_loss + pseudo_loss + kl_loss
@@ -113,9 +139,16 @@ def trainBPL(args):
                     # exponential decay
                     param_group["lr"] = args.lr * ((1 - float(step) / args.iterations) ** 0.99)
 
+                validate_acc = validate(validate_loader=val_labelled_data_loader,
+                                        model=model,
+                                        no_validate=args.validate_no,
+                                        full_orthogonal=args.full_orthogonal)
+
                 print(
                     'Step [{}/{}], '
                     'lr: {:.4f},'
+                    'train iou: {:.4f},'
+                    'val iou: {:.4f},'
                     'loss d: {:.4f}, '
                     'loss h: {:.4f}, '
                     'loss w: {:.4f}, '
@@ -124,6 +157,8 @@ def trainBPL(args):
                     'Threshold: {:.4f}'.format(step + 1,
                                                args.iterations,
                                                optimizer.param_groups[0]["lr"],
+                                               train_iou,
+                                               validate_acc,
                                                loss_d.get('supervised loss').get('loss').mean().item(),
                                                loss_h.get('supervised loss').get('loss').mean().item(),
                                                loss_w.get('supervised loss').get('loss').mean().item(),
@@ -144,6 +179,9 @@ def trainBPL(args):
                                                     'learnt threshold': learnt_threshold,
                                                     'train kl loss': kl_loss}, step + 1)
 
+                writer.add_scalars('ious', {'train iu': train_iou,
+                                            'val iu': validate_acc}, step + 1)
+
         elif args.full_orthogonal == 0:
 
             loss_o = train_semi(labelled_img=labelled_dict["plane"][0],
@@ -152,15 +190,16 @@ def trainBPL(args):
                                 model=model,
                                 t=args.temp,
                                 prior_mu=args.mu,
-                                prior_logsigma=args.sigma,
+                                # prior_logsigma=args.sigma,
                                 augmentation_cutout=args.cutout)
 
             sup_loss = loss_o.get('supervised loss').get('loss').mean()
+            train_iou = loss_o.get('supervised loss').get('train iou')
             # print(sup_loss)
 
             pseudo_loss = current_alpha*loss_o.get('pseudo loss').get('loss').mean()
 
-            kl_loss = current_beta*loss_o.get('kl loss').get('loss').mean()
+            kl_loss = current_alpha*current_beta*loss_o.get('kl loss').get('loss').mean()
 
             loss = sup_loss + pseudo_loss + kl_loss
 
@@ -178,15 +217,24 @@ def trainBPL(args):
                     # exponential decay
                     param_group["lr"] = args.lr * ((1 - float(step) / args.iterations) ** 0.99)
 
+                validate_acc = validate(validate_loader=val_labelled_data_loader,
+                                        model=model,
+                                        no_validate=args.validate_no,
+                                        full_orthogonal=args.full_orthogonal)
+
                 print(
                     'Step [{}/{}], '
                     'lr: {:.4f},'
+                    'train iou: {:.4f},'
+                    'val iou: {:.4f},'
                     'loss: {:.4f}, '
                     'pseudo loss: {:.4f}, '
                     'kl loss: {:.4f}, '
                     'Threshold: {:.4f}'.format(step + 1,
                                                args.iterations,
                                                optimizer.param_groups[0]["lr"],
+                                               train_iou,
+                                               validate_acc,
                                                sup_loss.item(),
                                                pseudo_loss.item(),
                                                kl_loss.item(),
@@ -201,29 +249,43 @@ def trainBPL(args):
                                                     'train pseudo loss': pseudo_loss,
                                                     'learnt threshold': learnt_threshold,
                                                     'train kl loss': kl_loss}, step + 1)
+                writer.add_scalars('ious', {'train iu': train_iou,
+                                            'val iu': validate_acc}, step + 1)
 
-        if step > args.saving_starting and step % args.saving_frequency == 0:
-            save_model_name_full = saved_model_path + '/' + model_name + '_' + str(step) + '.pt'
+        if step > args.ema_saving_starting:
+            ema_count += 1
+            if (step - args.ema_saving_starting) == 1:
+                for ema_param, param in zip(model_ema.parameters(), model.parameters()):
+                    ema_param.data = param.data
+            else:
+                for ema_param, param in zip(model_ema.parameters(), model.parameters()):
+                    ema_param.data.add_(param.data)
+
+        if validate_acc > best_val:
+            save_model_name_full = saved_model_path + '/' + model_name + '_best_val.pt'
             torch.save(model, save_model_name_full)
-
-        elif step > args.iterations - 100 and step % 2 == 0:
-            save_model_name_full = saved_model_path + '/' + model_name + '_' + str(step) + '.pt'
-            torch.save(model, save_model_name_full)
-
-    save_model_name_full = saved_model_path + '/' + model_name + '_' + str(args.iterations) + '.pt'
-    torch.save(model, save_model_name_full)
+            best_val = max(best_val, validate_acc)
+        else:
+            best_val_count += 1
+            best_val = best_val
+            if best_val_count > args.patience:
+                for ema_param in model_ema.parameters():
+                    ema_param = ema_param / ema_count
+                save_model_name_full = saved_model_path + '/' + model_name + '_ema.pt'
+                torch.save(model_ema, save_model_name_full)
+                break
 
     stop = timeit.default_timer()
     training_time = stop - start
     print('Training Time: ', training_time)
 
-    save_path = saved_model_path + '/results'
-    Path(save_path).mkdir(parents=True, exist_ok=True)
-    print('\nTraining finished and model saved\n')
+    # save_path = saved_model_path + '/results'
+    # Path(save_path).mkdir(parents=True, exist_ok=True)
+    # print('\nTraining finished and model saved\n')
 
-    # zip all models:
-    shutil.make_archive(saved_model_path, 'zip', saved_model_path)
-    shutil.rmtree(saved_model_path)
+    # # zip all models:
+    # shutil.make_archive(saved_model_path, 'zip', saved_model_path)
+    # shutil.rmtree(saved_model_path)
 
 
 

MainSup.py
@@ -7,6 +7,8 @@ import math
 from libs.Train import train_sup
 from libs import Helpers
 
+from libs.Validate import validate
+
 
 def trainSup(args):
     # fix a random seed:
@@ -14,6 +16,7 @@ def trainSup(args):
 
     # model intialisation:
     model, model_name = Helpers.network_intialisation(args)
+    model_ema, _ = Helpers.network_intialisation(args)
 
     # resume training:
     if args.resume is True:
@@ -21,6 +24,7 @@ def trainSup(args):
 
     # put model in the gpu:
     model.cuda()
+    model_ema.cuda()
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-8, weight_decay=args.l2)
 
     # make saving directories:
@@ -36,8 +40,24 @@ def trainSup(args):
     train_labelled_data_loader = data_iterators.get('train_loader_l')
     iterator_train_labelled = iter(train_labelled_data_loader)
 
+    # validate labelled:
+    val_labelled_data_loader = data_iterators.get('val_loader_l')
+
+    # initialisation of best acc tracker
+    best_val = 0.0
+
+    # initialisation of counter for ema avg:
+    ema_count = 0
+
+    # initialisation of growth of val acc tracker
+    best_val_count = 1
+
     # running loop:
     for step in range(args.iterations):
+
+        # initialisation of validating acc:
+        validate_acc = 0.0
+
         # put model to training mode:
         model.train()
 
@@ -45,26 +65,29 @@ def trainSup(args):
         labelled_dict = Helpers.get_data_dict(train_labelled_data_loader, iterator_train_labelled)
 
         if args.full_orthogonal == 1:
-            loss_d = train_sup(labelled_img=labelled_dict["plaine_d"][0],
-                               labelled_label=labelled_dict["plaine_d"][1],
+            loss_d = train_sup(labelled_img=labelled_dict["plane_d"][0],
+                               labelled_label=labelled_dict["plane_d"][1],
                                model=model,
                                t=args.temp,
                                augmentation_cutout=args.cutout)
 
-            loss_h = train_sup(labelled_img=labelled_dict["plaine_h"][0],
-                               labelled_label=labelled_dict["plaine_h"][1],
+            loss_h = train_sup(labelled_img=labelled_dict["plane_h"][0],
+                               labelled_label=labelled_dict["plane_h"][1],
                                model=model,
                                t=args.temp,
                                augmentation_cutout=args.cutout)
 
-            loss_w = train_sup(labelled_img=labelled_dict["plaine_w"][0],
-                               labelled_label=labelled_dict["plaine_w"][1],
+            loss_w = train_sup(labelled_img=labelled_dict["plane_w"][0],
+                               labelled_label=labelled_dict["plane_w"][1],
                                model=model,
                                t=args.temp,
                                augmentation_cutout=args.cutout)
 
             loss = loss_d.get('supervised loss').get('loss') + loss_h.get('supervised loss').get('loss') + loss_w.get('supervised loss').get('loss')
             loss = loss.mean() / 3
+
+            train_iou = loss_d.get('supervised loss').get('train iou') + loss_h.get('supervised loss').get('train iou') + loss_w.get('supervised loss').get('train iou')
+            train_iou = train_iou / 3
 
             del labelled_dict
 
@@ -77,17 +100,24 @@ def trainSup(args):
                     # exponential decay
                     param_group["lr"] = args.lr * ((1 - float(step) / args.iterations) ** 0.99)
 
+                validate_acc = validate(validate_loader=val_labelled_data_loader,
+                                        model=model,
+                                        no_validate=args.validate_no,
+                                        full_orthogonal=args.full_orthogonal)
+
                 print(
                     'Step [{}/{}], '
                     'lr: {:.4f},'
+                    'val iou: {:.4f},'
                     'Train loss d: {:.4f}, '
                     'Train loss h: {:.4f}, '
                     'Train loss w: {:.4f}, '.format(step + 1,
                                                     args.iterations,
                                                     optimizer.param_groups[0]["lr"],
+                                                    validate_acc,
                                                     loss_d.get('supervised loss').get('loss').mean().item(),
                                                     loss_h.get('supervised loss').get('loss').mean().item(),
-                                                    loss_w.get('supervised loss').get('loss').mean().item(),))
+                                                    loss_w.get('supervised loss').get('loss').mean().item()))
 
                 # # # ================================================================== #
                 # # #                        TensorboardX Logging                        #
@@ -98,6 +128,9 @@ def trainSup(args):
                                                     'train seg loss w': loss_w.get('supervised loss').get('loss').mean().item(),
                                                     }, step + 1)
 
+                writer.add_scalars('ious', {'train iu': train_iou,
+                                            'val iu': validate_acc}, step + 1)
+
         elif args.full_orthogonal == 0:
             loss_o = train_sup(labelled_img=labelled_dict["plane"][0],
                                labelled_label=labelled_dict["plane"][1],
@@ -106,6 +139,7 @@ def trainSup(args):
                                augmentation_cutout=args.cutout)
 
             loss = loss_o.get('supervised loss').get('loss').mean()
+            train_iou = loss_o.get('supervised loss').get('train iou')
 
             del labelled_dict
 
@@ -118,42 +152,59 @@ def trainSup(args):
                     # exponential decay
                     param_group["lr"] = args.lr * ((1 - float(step) / args.iterations) ** 0.99)
 
+                validate_acc = validate(validate_loader=val_labelled_data_loader,
+                                        model=model,
+                                        no_validate=args.validate_no,
+                                        full_orthogonal=args.full_orthogonal)
+
                 print(
                     'Step [{}/{}], '
                     'lr: {:.4f},'
+                    'val iou: {:.4f},'
                     'Train loss: {:.4f}, '.format(step + 1,
-                                                    args.iterations,
-                                                    optimizer.param_groups[0]["lr"],
-                                                    loss))
+                                                  args.iterations,
+                                                  optimizer.param_groups[0]["lr"],
+                                                  validate_acc,
+                                                  loss))
 
                 # # # ================================================================== #
                 # # #                        TensorboardX Logging                        #
                 # # # # ================================================================ #
 
                 writer.add_scalars('loss metrics', {'train loss': loss.get('supervised loss')}, step + 1)
+                writer.add_scalars('ious', {'train iu': train_iou,
+                                            'val iu': validate_acc}, step + 1)
 
-        if step > args.saving_starting and step % args.saving_frequency == 0:
-            save_model_name_full = saved_model_path + '/' + model_name + '_' + str(step) + '.pt'
+        if step > args.ema_saving_starting:
+            ema_count += 1
+            for ema_param, param in zip(model_ema.parameters(), model.parameters()):
+                ema_param.data.add_(param.data)
+
+        if validate_acc > best_val:
+            save_model_name_full = saved_model_path + '/' + model_name + '_best_val.pt'
             torch.save(model, save_model_name_full)
-
-        elif step > args.iterations - 100 and step % 2 == 0:
-            save_model_name_full = saved_model_path + '/' + model_name + '_' + str(step) + '.pt'
-            torch.save(model, save_model_name_full)
-
-    save_model_name_full = saved_model_path + '/' + model_name + '_' + str(args.iterations) + '.pt'
-    torch.save(model, save_model_name_full)
+            best_val = max(best_val, validate_acc)
+        else:
+            best_val_count += 1
+            best_val = best_val
+            if best_val_count > args.patience:
+                for ema_param in model_ema.parameters():
+                    ema_param /= ema_count
+                save_model_name_full = saved_model_path + '/' + model_name + '_ema.pt'
+                torch.save(model_ema, save_model_name_full)
+                break
 
     stop = timeit.default_timer()
     training_time = stop - start
     print('Training Time: ', training_time)
 
-    save_path = saved_model_path + '/results'
-    Path(save_path).mkdir(parents=True, exist_ok=True)
-    print('\nTraining finished and model saved\n')
-
-    # zip all models:
-    shutil.make_archive(saved_model_path, 'zip', saved_model_path)
-    shutil.rmtree(saved_model_path)
+    # save_path = saved_model_path + '/results'
+    # Path(save_path).mkdir(parents=True, exist_ok=True)
+    # print('\nTraining finished and model saved\n')
+    #
+    # # zip all models:
+    # shutil.make_archive(saved_model_path, 'zip', saved_model_path)
+    # shutil.rmtree(saved_model_path)
 
 
 

Metrics.py
@@ -100,21 +100,21 @@ def segmentation_scores(label_trues, label_preds, n_class):
     hist = np.zeros((n_class, n_class))
     for lt, lp in zip(label_trues, label_preds):
         hist += _fast_hist(lt.flatten(), lp.flatten(), n_class)
-    acc = np.diag(hist).sum() / hist.sum()
-    acc_cls = np.diag(hist) / hist.sum(axis=1)
-    acc_cls = np.nanmean(acc_cls)
+    # acc = np.diag(hist).sum() / hist.sum()
+    # acc_cls = np.diag(hist) / hist.sum(axis=1)
+    # acc_cls = np.nanmean(acc_cls)
     # iou:
     iu = np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist) + 1e-10)
     mean_iou = np.nanmean(iu)
 
-    freq = hist.sum(axis=1) / hist.sum()
-    fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
+    # freq = hist.sum(axis=1) / hist.sum()
+    # fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
     # iflat = label_preds.view(-1)
     # tflat = label_trues.view(-1)
     # intersection = (iflat * tflat).sum()
     # union = iflat.sum() + tflat.sum()
 
-    return mean_iou, acc_cls, fwavacc
+    return mean_iou
 # ==================================================================================
 
 

Models2D.py
@@ -14,11 +14,20 @@ class ThresholdEncoder(nn.Module):
         '''
         super(ThresholdEncoder, self).__init__()
 
+        # self.network = nn.Sequential(
+        #     nn.Conv2d(in_channels=c, out_channels=c*ratio, kernel_size=(3, 3), stride=(1, 1), dilation=(1, 1), padding=(1, 1), bias=False),
+        #     nn.InstanceNorm2d(c*ratio, affine=True),
+        #     nn.PReLU(),
+        #     nn.Conv2d(in_channels=c*ratio, out_channels=c * ratio, kernel_size=(3, 3), stride=(1, 1), dilation=(1, 1), padding=(1, 1), bias=False),
+        #     nn.InstanceNorm2d(c * ratio, affine=True),
+        #     nn.PReLU()
+        # )
+
         self.network = nn.Sequential(
-            nn.Conv2d(in_channels=c, out_channels=c*ratio, kernel_size=(3, 3), stride=(1, 1), dilation=(1, 1), padding=(1, 1), bias=False),
+            nn.Conv2d(in_channels=c, out_channels=c*ratio, kernel_size=(1, 1), stride=(1, 1), dilation=(1, 1), padding=(0, 0), bias=False),
             nn.InstanceNorm2d(c*ratio, affine=True),
             nn.PReLU(),
-            nn.Conv2d(in_channels=c*ratio, out_channels=c * ratio, kernel_size=(3, 3), stride=(1, 1), dilation=(1, 1), padding=(1, 1), bias=False),
+            nn.Conv2d(in_channels=c*ratio, out_channels=c * ratio, kernel_size=(1, 1), stride=(1, 1), dilation=(1, 1), padding=(0, 0), bias=False),
             nn.InstanceNorm2d(c * ratio, affine=True),
             nn.PReLU()
         )
@@ -26,11 +35,12 @@ class ThresholdEncoder(nn.Module):
         self.threshold_logvar = nn.Conv2d(in_channels=ratio*c, out_channels=1, kernel_size=(1, 1), stride=(1, 1), dilation=(1, 1), padding=(0, 0), bias=True)
         self.threshold_mean = nn.Conv2d(in_channels=ratio*c, out_channels=1, kernel_size=(1, 1), stride=(1, 1), dilation=(1, 1), padding=(0, 0), bias=True)
 
-        self.softplus = nn.Softplus()
+        # self.softplus = nn.Softplus()
 
     def forward(self, x):
         y = self.network(x)
-        return self.softplus(self.threshold_mean(y)), self.threshold_logvar(y)
+        # return self.softplus(self.threshold_mean(y)), self.threshold_logvar(y)
+        return self.threshold_mean(y), self.threshold_logvar(y)
 
 
 # class ThresholdDecoder(nn.Module):
@@ -69,7 +79,8 @@ class UnetBPL(nn.Module):
                  out_ch,
                  norm='in',
                  ratio=8,
-                 detach=True):
+                 # detach=True
+                 ):
         '''
         Args:
             in_ch:
@@ -83,9 +94,10 @@ class UnetBPL(nn.Module):
         super(UnetBPL, self).__init__()
         if out_ch == 2:
             out_ch = 1
-        self.detach_bpl = detach
+        # self.detach_bpl = detach
         self.segmentor = Unet(in_ch, width, depth, out_ch, norm=norm, side_output=True)
         self.encoder = ThresholdEncoder(c=width, ratio=ratio)
+        self.avg_pool = nn.AvgPool2d(1)
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
@@ -95,12 +107,17 @@ class UnetBPL(nn.Module):
     def forward(self, x):
         outputs_dict = self.segmentor(x)
         output, threshold_input = outputs_dict.get('segmentation'), outputs_dict.get('side_output')
-        if self.detach_bpl is True:
-            mu, logvar = self.encoder(threshold_input.detach())
-        else:
-            mu, logvar = self.encoder(threshold_input)
+
+        # if self.detach_bpl == 1:
+        #     mu, logvar = self.encoder(threshold_input.detach())
+        # else:
+        #     mu, logvar = self.encoder(threshold_input)
+
+        mu, logvar = self.encoder(self.avg_pool(threshold_input))
 
         learnt_threshold = self.reparameterize(mu, logvar)
+        # learnt_threshold = F.softplus(learnt_threshold)
+        # learnt_threshold = torch.sigmoid(learnt_threshold)
 
         return {
             'segmentation': output,

libs/Augmentations.py
@@ -57,101 +57,34 @@ class RandomZoom(object):
         return image_zoomed, label_zoomed
 
 
-class RandomSlicingOrthogonalFast(object):
-    def __init__(self,
-                 # input_size=(140, 140, 140),  # crop size on the input volume: d x h x w
-                 output_size=(160, 160),  # new size of output augmented volume: d x h x w
-                 discarded_slices=5,
-                 zoom=1):
-
-        self.discarded_slices = discarded_slices
-        self.zoom = zoom
-
-        self.new_size_h = output_size[0]
-        self.new_size_w = output_size[1]
-        # self.new_size_w = output_size[2]
-
-        # self.input_size_d = input_size[0]
-        # self.input_size_h = input_size[1]
-        # self.input_size_w = input_size[2]
-
-        if self.zoom == 1:
-            self.zoom_aug = RandomZoom()
-
-    def crop(self, *volumes):
-
-        outputs = {"plane": []}
-
-        d, h, w = np.shape(volumes[0])
-
-        sample_position_d = random.randint(0, d-1)
-        sample_position_h = random.randint(0, h-1)
-        sample_position_w = random.randint(0, w-1)
-
-        # assert d >= self.new_size_d
-        # assert h >= self.new_size_h
-        # assert w >= self.new_size_w
-
-        roll_a_dice = random.random()
-
-        if roll_a_dice < 0.34:
-            for i, each_input in enumerate(volumes):
-                if i == 0:
-                    outputs["plane"].append(resize(np.squeeze(each_input[sample_position_d, :, :]), (self.new_size_h, self.new_size_w), order=1))
-                else:
-                    outputs["plane"].append(resize(np.squeeze(each_input[sample_position_d, :, :]), (self.new_size_h, self.new_size_w), order=0))
-
-        elif roll_a_dice < 0.68:
-            for i, each_input in enumerate(volumes):
-                if i == 0:
-                    outputs["plane"].append(resize(np.squeeze(each_input[:, sample_position_h, :]), (self.new_size_h, self.new_size_w), order=1))
-                else:
-                    outputs["plane"].append(resize(np.squeeze(each_input[:, sample_position_h, :]), (self.new_size_h, self.new_size_w), order=0))
-
-        else:
-            for i, each_input in enumerate(volumes):
-                if i == 0:
-                    outputs["plane"].append(resize(np.squeeze(each_input[:, :, sample_position_w]), (self.new_size_h, self.new_size_w), order=1))
-                else:
-                    outputs["plane"].append(resize(np.squeeze(each_input[:, :, sample_position_w]), (self.new_size_h, self.new_size_w), order=0))
-
-        if self.zoom == 1:
-            if random.random() >= 0.5:
-                outputs["plane"][0], outputs["plane"][1] = self.zoom_aug.forward(outputs["plane"][0], outputs["plane"][1])
-
-        return outputs
-
-
 class RandomSlicingOrthogonal(object):
     def __init__(self,
-                 new_size_d=384,
-                 new_size_h=384,
-                 new_size_w=384,
+                 output_size=(160, 160),
                  full_orthogonal=0,
-                 discarded_slices=5,
+                 # discarded_slices=5,
                  zoom=1,
-                 sampling_weighting_slope=0):
+                 # sampling_weighting_slope=0
+                 ):
         '''
         cropping_d: 3 d dimension of cropped sub volume cropping on h x w
         cropping_h: 3 d dimension of cropped sub volume cropping on w x d
         cropping_w: 3 d dimension of cropped sub volume cropping on h x d
         '''
-        self.discarded_slices = discarded_slices
+        # self.discarded_slices = discarded_slices
         self.zoom = zoom
         self.full_orthogonal = full_orthogonal
 
-        self.new_size_d = new_size_d
-        self.new_size_h = new_size_h
-        self.new_size_w = new_size_w
+        self.new_size_h = output_size[0]
+        self.new_size_w = output_size[1]
 
         # Over sampling the slices on the two ends because they contain small difficult vessels
         # We give the middle slice the lowest weight and the slices at the two very ends the highest weights
 
-        weight_middle_slice = 1
-        default_no_slices = 512
-        weight_end_slices = sampling_weighting_slope*0.5*default_no_slices + weight_middle_slice
-        sampling_weights_prob = [int(abs((i-0.5*default_no_slices))*sampling_weighting_slope+default_no_slices) / weight_end_slices for i in range(default_no_slices)]
-        self.sampling_weights_prob = [i / sum(sampling_weights_prob) for i in sampling_weights_prob] # normalise so everything adds up to 1
+        # weight_middle_slice = 1
+        # default_no_slices = 512
+        # weight_end_slices = sampling_weighting_slope*0.5*default_no_slices + weight_middle_slice
+        # sampling_weights_prob = [int(abs((i-0.5*default_no_slices))*sampling_weighting_slope+default_no_slices) / weight_end_slices for i in range(default_no_slices)]
+        # self.sampling_weights_prob = [i / sum(sampling_weights_prob) for i in sampling_weights_prob] # normalise so everything adds up to 1
 
         if self.zoom == 1:
             self.zoom_aug = RandomZoom()
@@ -164,25 +97,29 @@ class RandomSlicingOrthogonal(object):
                        "plane_w": []}
 
             d, h, w = np.shape(volumes[0])
-            sample_position_d_d = np.random.choice(np.arange(d), 1, p=self.sampling_weights_prob)
-            sample_position_h_h = np.random.choice(np.arange(h), 1, p=self.sampling_weights_prob)
-            sample_position_w_w = np.random.choice(np.arange(w), 1, p=self.sampling_weights_prob)
+            # sample_position_d_d = np.random.choice(np.arange(d), 1, p=self.sampling_weights_prob)
+            # sample_position_h_h = np.random.choice(np.arange(h), 1, p=self.sampling_weights_prob)
+            # sample_position_w_w = np.random.choice(np.arange(w), 1, p=self.sampling_weights_prob)
 
-            if d > self.new_size_d:
-                d_h = d - self.new_size_d
-                sample_position_d_h = np.random.choice(np.arange(d_h), 1)
-            else:
-                sample_position_d_h = 0
+            sample_position_d = random.randint(0, d - 1)
+            sample_position_h = random.randint(0, h - 1)
+            sample_position_w = random.randint(0, w - 1)
+
+            # if d > self.new_size_d:
+            #     d_h = d - self.new_size_d
+            #     sample_position_d_h = np.random.choice(np.arange(d_h), 1)
+            # else:
+            #     sample_position_d_h = 0
 
             for i, each_input in enumerate(volumes):
                 if i == 0:
-                    outputs["plane_d"].append(resize(np.squeeze(each_input[sample_position_d_d, :, :]), (self.new_size_h, self.new_size_w), order=1))
-                    outputs["plane_h"].append(resize(np.squeeze(each_input[sample_position_d_h:sample_position_d_h+self.new_size_d, sample_position_h_h, :]), (self.new_size_h, self.new_size_w), order=1))
-                    outputs["plane_w"].append(resize(np.squeeze(each_input[sample_position_d_h:sample_position_d_h+self.new_size_d, :, sample_position_w_w]), (self.new_size_h, self.new_size_w), order=1))
+                    outputs["plane_d"].append(resize(np.squeeze(each_input[sample_position_d, :, :]), (self.new_size_h, self.new_size_w), order=1))
+                    outputs["plane_h"].append(resize(np.squeeze(each_input[:, sample_position_h, :]), (self.new_size_h, self.new_size_w), order=1))
+                    outputs["plane_w"].append(resize(np.squeeze(each_input[:, :, sample_position_w]), (self.new_size_h, self.new_size_w), order=1))
                 elif i == 1:
-                    outputs["plane_d"].append(resize(np.squeeze(each_input[sample_position_d_d, :, :]), (self.new_size_h, self.new_size_w), order=0))
-                    outputs["plane_h"].append(resize(np.squeeze(each_input[sample_position_d_h:sample_position_d_h+self.new_size_d, sample_position_h_h, :]), (self.new_size_h, self.new_size_w), order=0))
-                    outputs["plane_w"].append(resize(np.squeeze(each_input[sample_position_d_h:sample_position_d_h+self.new_size_d, :, sample_position_w_w]), (self.new_size_h, self.new_size_w), order=0))
+                    outputs["plane_d"].append(resize(np.squeeze(each_input[sample_position_d, :, :]), (self.new_size_h, self.new_size_w), order=0))
+                    outputs["plane_h"].append(resize(np.squeeze(each_input[:, sample_position_h, :]), (self.new_size_h, self.new_size_w), order=0))
+                    outputs["plane_w"].append(resize(np.squeeze(each_input[:, :, sample_position_w]), (self.new_size_h, self.new_size_w), order=0))
 
             if self.zoom is True:
                 if random.random() >= 0.5:
@@ -195,23 +132,19 @@ class RandomSlicingOrthogonal(object):
         elif self.full_orthogonal == 0:
             outputs = {"plane": []}
 
-            newd, newh, neww = np.shape(volumes[0])
+            d, h, w = np.shape(volumes[0])
 
-            sample_position_d = np.random.choice(np.arange(newd), 1, p=self.sampling_weights_prob)
-            sample_position_h = np.random.choice(np.arange(newh), 1, p=self.sampling_weights_prob)
-            sample_position_w = np.random.choice(np.arange(neww), 1, p=self.sampling_weights_prob)
+            # sample_position_d = np.random.choice(np.arange(newd), 1, p=self.sampling_weights_prob)
+            # sample_position_h = np.random.choice(np.arange(newh), 1, p=self.sampling_weights_prob)
+            # sample_position_w = np.random.choice(np.arange(neww), 1, p=self.sampling_weights_prob)
 
-            # roll_a_dice = random.random()
-            #
-            # if roll_a_dice < 0.34:
-            #     for each_input in volumes:
-            #         outputs["plane"].append(resize(np.squeeze(each_input[sample_position_d_d, :, :]), (self.new_size_h, self.new_size_w)))
-            # elif roll_a_dice < 0.68:
-            #     for each_input in volumes:
-            #         outputs["plane"].append(resize(np.squeeze(each_input[:, sample_position_h_h, :]), (self.new_size_h, self.new_size_w)))
-            # else:
-            #     for each_input in volumes:
-            #         outputs["plane"].append(resize(np.squeeze(each_input[:, :, sample_position_w_w]), (self.new_size_h, self.new_size_w)))
+            sample_position_d = random.randint(0, d - 1)
+            sample_position_h = random.randint(0, h - 1)
+            sample_position_w = random.randint(0, w - 1)
+
+            # assert d >= self.new_size_d
+            # assert h >= self.new_size_h
+            # assert w >= self.new_size_w
 
             roll_a_dice = random.random()
 

libs/Train.py
@@ -64,12 +64,12 @@ def calculate_sup_loss(lbl,
 
     if torch.sum(lbl) > 10: # check whether there are enough foreground pixels
         prob_output = outputs_dict.get('segmentation') # get segmentation map
-        if b_u > 0: # check if unlabelled data is included
+        if b_u: # check if unlabelled data is included
             prob_output, _ = torch.split(prob_output, [b_l, b_u], dim=0) # if both labelled and unlabelled, split the data and use the labelled
 
         prob_output = torch.sigmoid(prob_output / temp) # apply element-wise sigmoid
 
-        if cutout_aug is True: # apply cutout augmentation
+        if cutout_aug == 1: # apply cutout augmentation
             prob_output, lbl = randomcutout(prob_output, lbl)
 
         # channel-wise loss (this is for multi-channel sigmoid function as well):
@@ -78,7 +78,6 @@ def calculate_sup_loss(lbl,
             loss = SoftDiceLoss()(prob_output, lbl) + nn.BCELoss(reduction='mean')(prob_output.squeeze() + 1e-10, lbl.squeeze() + 1e-10)
             class_outputs = (prob_output > 0.95).float()
             train_mean_iu_ = segmentation_scores(lbl, class_outputs, 2)
-            train_mean_iu_ = sum(train_mean_iu_) / len(train_mean_iu_)
             return {'loss': loss.mean(),
                     'train iou': train_mean_iu_}
 
@@ -88,7 +87,6 @@ def calculate_sup_loss(lbl,
                 loss = SoftDiceLoss()(prob_output, lbl) + nn.BCELoss(reduction='mean')(prob_output.squeeze() + 1e-10, lbl.squeeze() + 1e-10)
                 class_outputs = (prob_output > 0.95).float()
                 train_mean_iu_ = segmentation_scores(lbl, class_outputs, 2)
-                train_mean_iu_ = sum(train_mean_iu_) / len(train_mean_iu_)
                 return {'loss': loss.mean(),
                         'train iou': train_mean_iu_}
 
@@ -127,7 +125,8 @@ def calculate_kl_loss(outputs_dict,
                       b_u,
                       b_l,
                       prior_u,
-                      prior_var):
+                      # prior_var
+                      ):
 
     assert b_u > 0
     posterior_mu = outputs_dict.get('mu')
@@ -135,7 +134,8 @@ def calculate_kl_loss(outputs_dict,
 
     confidence_threshold_learnt = outputs_dict.get('learnt_threshold')
 
-    loss = kld_loss(posterior_mu, posterior_logvar, prior_u, prior_var)
+    # loss = kld_loss(posterior_mu, posterior_logvar, prior_u, prior_var)
+    loss = kld_loss(posterior_mu, posterior_logvar, prior_u)
 
     threshold_learnt_l, threshold_learnt_u = torch.split(confidence_threshold_learnt, [b_l, b_u], dim=0)
 
@@ -148,7 +148,7 @@ def calculate_pseudo_loss(outputs_dict,
                           b_u,
                           b_l,
                           temp,
-                          cutout_aug,
+                          cutout_aug=0,
                           conf_threshold='bayesian'):
 
     assert b_u > 0
@@ -160,24 +160,28 @@ def calculate_pseudo_loss(outputs_dict,
     else:
         threshold = 0.5
 
-    _, predictions_u = torch.split(predictions_all, [b_l, b_u], dim=0)
-    _, threshold_u = torch.split(threshold, [b_l, b_u], dim=0)
+    predictions_l, predictions_u = torch.split(predictions_all, [b_l, b_u], dim=0)
+    threshold_l, threshold_u = torch.split(threshold, [b_l, b_u], dim=0)
     prob_output_u = torch.sigmoid(predictions_u / temp)
-    pseudo_label_u = (prob_output_u > threshold_u).float()
+    pseudo_label_u = (prob_output_u >= threshold_u).float()
+    prob_output_l = torch.sigmoid(predictions_l / temp)
+    pseudo_label_l = (prob_output_l >= threshold_l).float()
 
-    if cutout_aug is True:
+    if cutout_aug == 1:
         prob_output_u, pseudo_label_u = randomcutout(prob_output_u, pseudo_label_u)
 
     if torch.sum(pseudo_label_u) > 10:
         if len(prob_output_u.size()) == 3:
             # this is binary segmentation
             loss = SoftDiceLoss()(prob_output_u, pseudo_label_u) + nn.BCELoss(reduction='mean')(prob_output_u.squeeze() + 1e-10, pseudo_label_u.squeeze() + 1e-10)
+            loss += 0.5*SoftDiceLoss()(prob_output_l, pseudo_label_l) + nn.BCELoss(reduction='mean')(prob_output_l.squeeze() + 1e-10, pseudo_label_l.squeeze() + 1e-10)
             return {'loss': loss.mean()}
 
         elif len(prob_output_u.size()) == 4:
             if prob_output_u.size()[1] == 1:
                 # this is also binary segmentation
                 loss = SoftDiceLoss()(prob_output_u, pseudo_label_u) + nn.BCELoss(reduction='mean')(prob_output_u.squeeze() + 1e-10, pseudo_label_u.squeeze() + 1e-10)
+                loss += 0.5*SoftDiceLoss()(prob_output_l, pseudo_label_l) + nn.BCELoss(reduction='mean')(prob_output_l.squeeze() + 1e-10, pseudo_label_l.squeeze() + 1e-10)
                 return {'loss': loss.mean()}
 
             else:
@@ -186,11 +190,12 @@ def calculate_pseudo_loss(outputs_dict,
                 loss = torch.tensor(0).to('cuda')
                 effective_classes = 0
                 for i in range(prob_output_u.size()[1]):  # multiple
-                    if torch.sum(pseudo_label_u[:, i, :, :]) > 1.0:
+                    if torch.sum(pseudo_label_u[:, i, :, :]) > 10.0:
                         # If the channel is not empty, we learn it otherwise we ignore that channel because sometimes we do learn some very weird stuff
                         # It is necessary to use this condition because some labels do not necessarily contain all of the classes in one image.
                         effective_classes += 1
                         loss += SoftDiceLoss()(prob_output_u[:, i, :, :], pseudo_label_u[:, i, :, :]).mean() + nn.BCELoss(reduction='mean')(prob_output_u[:, i, :, :].squeeze() + 1e-10, pseudo_label_u[:, i, :, :].squeeze() + 1e-10).mean()
+                        loss += 0.5*SoftDiceLoss()(prob_output_l[:, i, :, :], pseudo_label_l[:, i, :, :]).mean() + nn.BCELoss(reduction='mean')(prob_output_l[:, i, :, :].squeeze() + 1e-10, pseudo_label_l[:, i, :, :].squeeze() + 1e-10).mean()
                 loss = loss / effective_classes
                 return {'loss': loss.mean()}
 
@@ -202,7 +207,7 @@ def train_sup(labelled_img,
               labelled_label,
               model,
               t=2.0,
-              augmentation_cutout=True):
+              augmentation_cutout=0):
 
     inputs = np2tensor_all(**{'img_l':labelled_img, 'lbl':labelled_label})
     train_img = get_img(**inputs)
@@ -221,9 +226,9 @@ def train_semi(labelled_img,
                model,
                unlabelled_img,
                t=2.0,
-               prior_mu=0.6,
-               prior_logsigma=0.1,
-               augmentation_cutout=True):
+               prior_mu=0.7,
+               # prior_logsigma=0.1,
+               augmentation_cutout=0):
 
     # convert data from numpy to tensor:
     inputs = np2tensor_all(**{'img_l': labelled_img,
@@ -249,7 +254,8 @@ def train_semi(labelled_img,
                                 b_l=train_img.get('batch labelled'),
                                 b_u=train_img.get('batch unlabelled'),
                                 prior_u=prior_mu,
-                                prior_var=prior_logsigma)
+                                # prior_var=prior_logsigma
+                                )
 
     # pseudo label loss:
     pseudo_loss = calculate_pseudo_loss(outputs_dict=outputs_dict,