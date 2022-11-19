79530018e6fe3ba8124572520d9c89a26a4e5926
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-11-19 23:14:22 +0000

    add a few exps on cluster

--- Run.py
+++ Run.py
@@ -12,21 +12,21 @@ def main():
     # paths to the training data
     parser.add_argument('--data', type=str, default='/home/moucheng/projects_data/HipCT/COVID_ML_data/hip_covid', help='Data path')
     # parser.add_argument('--data', type=str, default='/home/moucheng/projects_data/Pulmonary_data/airway', help='Data path')
-    parser.add_argument('--log_tag', type=str, default='hip_ct_sup_aug', help='experiment tag for the record')
+    parser.add_argument('--log_tag', type=str, default='hip_ct_sup', help='experiment tag for the record')
 
     # hyper parameters for training (both sup and semi sup):
     parser.add_argument('--input_dim', type=int, help='dimension for the input image, e.g. 1 for CT, 3 for RGB, and more for 3D inputs', default=1)
     parser.add_argument('--output_dim', type=int, help='dimension for the output, e.g. 1 for binary segmentation, 3 for 3 classes', default=1)
-    parser.add_argument('--iterations', type=int, help='number of iterations', default=5000)
-    parser.add_argument('--lr', type=float, help='learning rate', default=0.01)
+    parser.add_argument('--iterations', type=int, help='number of iterations', default=20000)
+    parser.add_argument('--lr', type=float, help='learning rate', default=0.005)
     parser.add_argument('--width', type=int, help='number of filters in the first conv block in encoder', default=16)
     parser.add_argument('--depth', type=int, help='number of downsampling stages', default=4)
-    parser.add_argument('--batch', type=int, help='number of training batch size', default=2)
+    parser.add_argument('--batch', type=int, help='number of training batch size', default=4)
     parser.add_argument('--temp', '--t', type=float, help='temperature scaling on output logits when applying sigmoid and softmax', default=2.0)
     parser.add_argument('--l2', type=float, help='l2 normalisation', default=0.01)
     parser.add_argument('--seed', type=int, help='random seed', default=1128)
-    parser.add_argument('--ema_saving_starting', type=int, help='number of iterations when it starts to save avg model', default=100)
-    parser.add_argument('--patience', type=int, help='patience for validate accurate', default=100)
+    parser.add_argument('--ema_saving_starting', type=int, help='number of iterations when it starts to save avg model', default=200)
+    parser.add_argument('--patience', type=int, help='patience for validate accurate', default=20000)
     parser.add_argument('--validate_no', type=int, help='no of batch for validate because full validate is too time consuming', default=1)
 
     # hyper parameters for training (specific for semi sup)
@@ -39,8 +39,8 @@ def main():
 
     # flags for data preprocessing and augmentation in data loader:
     parser.add_argument('--gaussian', type=int, help='1 when add random gaussian noise', default=1)
-    parser.add_argument('--zoom', type=int, help='1 when use random zoom in augmentation', default=1)
-    parser.add_argument('--cutout', type=int, help='1 when randomly cutout some patches', default=1)
+    parser.add_argument('--zoom', type=int, help='1 when use random zoom in augmentation', default=0)
+    parser.add_argument('--cutout', type=int, help='1 when randomly cutout some patches', default=0)
     parser.add_argument('--contrast', type=int, help='1 when use random contrast using histogram equalization with random bins', default=1)
     parser.add_argument('--full_orthogonal', type=int, help='1 when each iteration has three orthogonal planes all together', default=1)
     parser.add_argument('--new_size_h', type=int, help='new size for the image height', default=448)
