38efae8106bfc3bda161ef3f3e23906e43a497a4
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-12-01 15:01:08 +0000

    trying to fix big file issue

--- cluster/covid04.sh
+++
@@ -1,8 +0,0 @@
-#$ -l tmem=16G
-#$ -l gpu=true,gpu_type=(rtx8000|a100)
-#$ -S /bin/bash
-#$ -j y
-#$ -l h_rt=168:00:00
-#$ -wd /SAN/medic/PerceptronHead/codes/SatsumaSeg/
-
-~/miniconda3/envs/pytorch1.4/bin/python Run.py --data '/SAN/medic/PerceptronHead/data/hipct_covid/class1.0' --log_tag 'class1' --unlabelled 4
