e20c423f55e87aba161437890b739afb04fcb48e
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-12-09 13:01:17 +0000

    2022 12 09b: use larger resolution 480

---
+++ cluster/airway03.sh
@@ -0,0 +1,8 @@
+#$ -l tmem=16G
+#$ -l gpu=true,gpu_type=(rtx8000|a100_80)
+#$ -S /bin/bash
+#$ -j y
+#$ -l h_rt=168:00:00
+#$ -wd /SAN/medic/PerceptronHead/codes/SatsumaSeg/
+
+~/miniconda3/envs/pytorch1.4/bin/python Run.py --data '/SAN/medic/PerceptronHead/data/airway' --log_tag 'airway' --format 'nii' --iterations 832000 --lr 1e-4 --depth 4 --width 32 --batch 4 --full_orthogonal 1 --new_size_h 480 --new_size_w 480 --patience 16640 --ema_saving_starting 166400 --unlabelled 2 --warmup_start 33280
