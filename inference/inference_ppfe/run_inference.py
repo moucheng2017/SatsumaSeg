da067b7f8e3fb5e5160272d6e5cfd28d04f287e1
moucheng2017 <rmapmxu@ucl.ac.uk>
2023-01-31 15:48:18 +0000

    2023 01 31g: new configurations

--- inference/inference_ppfe/run_inference.py
+++ inference/inference_ppfe/run_inference.py
@@ -49,42 +49,54 @@ if __name__ == '__main__':
 
     img = (img - np.nanmean(img) + 1e-10) / (np.nanstd(img) + 1e-10)
 
-    imgs_d = np.split(img, d // new_dim, axis=0)
+    # imgs_d = np.split(img, d // new_dim, axis=0)
 
     count = 0
 
-    for i, each_img_d in enumerate(imgs_d):
-        imgs_d_h = np.split(each_img_d, h // new_dim, axis=1)
-        for j, each_img_h in enumerate(imgs_d_h):
-            imgs_d_h_w = np.split(each_img_h, w // new_dim, axis=2)
-            for k, each_img_w in enumerate(imgs_d_h_w):
-                d_, h_, w_ = np.shape(each_img_w)
+    # for i, each_img_d in enumerate(imgs_d):
+    #     imgs_d_h = np.split(each_img_d, h // new_dim, axis=1)
+    #     for j, each_img_h in enumerate(imgs_d_h):
+    #         imgs_d_h_w = np.split(each_img_h, w // new_dim, axis=2)
+    #         for k, each_img_w in enumerate(imgs_d_h_w):
+    #             d_, h_, w_ = np.shape(each_img_w)
+    #
+    #             assert d_ == new_dim
+    #             assert h_ == new_dim
+    #             assert w_ == new_dim
+    #
+    #             seg_ = torch.from_numpy(each_img_w).cuda().unsqueeze(0).unsqueeze(0).float()
+    #             seg_ = model(seg_)
+    #             seg_ = seg_.get('segmentation')
+    #             _, seg_ = torch.max(seg_, dim=1)
+    #             seg_ = seg_.detach().cpu().numpy()
+    #             seg[i*new_dim:(i+1)*new_dim, j*new_dim:(j+1)*new_dim, k*new_dim:(k+1)*new_dim] = seg_
+    #
+    #             count += 1
+    #             print(count)
+    #
+    #             # seg_name = str(args.new_dim) + '_' + str(count) + '_segmentation.npy'
+    #             # save_file = os.path.join(args.save_path, seg_name)
+    #             # np.save(seg_, save_file)
+    #             # del each_img_w
+    #             # del seg_
 
-                assert d_ == new_dim
-                assert h_ == new_dim
-                assert w_ == new_dim
-
-                seg_ = torch.from_numpy(each_img_w).cuda().unsqueeze(0).unsqueeze(0).float()
+    for i in range(0, d - new_dim, 16):
+        for j in range(0, h - new_dim, 16):
+            for k in range(0, w - new_dim, 16):
+                input_volume = img[i:i+new_dim, j:j+new_dim, k:k+new_dim]
+                seg_ = torch.from_numpy(input_volume).cuda().unsqueeze(0).unsqueeze(0).float()
                 seg_ = model(seg_)
                 seg_ = seg_.get('segmentation')
                 _, seg_ = torch.max(seg_, dim=1)
                 seg_ = seg_.detach().cpu().numpy()
-                seg[i*new_dim:(i+1)*new_dim, j*new_dim:(j+1)*new_dim, k*new_dim:(k+1)*new_dim] = seg_
-
+                seg[i:i+new_dim, j:j+new_dim, k:k+new_dim] = seg_
                 count += 1
                 print(count)
-
-                # seg_name = str(args.new_dim) + '_' + str(count) + '_segmentation.npy'
-                # save_file = os.path.join(args.save_path, seg_name)
-                # np.save(seg_, save_file)
-                # del each_img_w
-                # del seg_
 
     seg = nib.Nifti1Image(seg, affine=np.eye(4))
     seg_name = str(args.new_dim) + '_segmentation.nii'
     save_file = os.path.join(args.save_path, seg_name)
     nib.save(seg, save_file)
-
     print('Done')
 
 
